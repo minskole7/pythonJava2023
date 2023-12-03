e7719129a185863874eeae1d4a34e091db908d46
minskole7 <minskole7@gmail.com>
2023-12-03 11:54:43 +0530

    added

--- testcases/WebElementsDay2.java
+++ testcases/WebElementsDay2.java
@@ -1,5 +1,7 @@
 package testcases;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,45 +11,96 @@ public class WebElementsDay2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// Testcase one 
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Downloads\\chromedriver\\chromedriver\\chromedriver.exe");
+		// Done
+		// click()
+
+		// sendKeys()
+
+		// getText()
+
+		// getAttribute()
+
+		// clear()
+
+		// isEnabled()
+
+		// isDisplayed()
+
+		// isSelected()
+
+		// submit() // Tagname => form
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\LENOVO\\Downloads\\chromedriver\\chromedriver\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+		driver.get("https://webdriveruniversity.com/Contact-Us/contactus.html");
+//		
+//		driver.findElement(By.cssSelector("[name= 'first_name']")).sendKeys("chinmay");
+//		driver.findElement(By.cssSelector("[name= 'last_name']")).sendKeys("deshpande");
+//		driver.findElement(By.cssSelector("[name= 'email']")).sendKeys("chinmaydeshpande010@gmail.com");
+//		driver.findElement(By.cssSelector("[name= 'message']")).sendKeys("I am learning js");
+//		driver.findElement(By.id("contact_form")).submit();
+//		boolean  avail = driver.findElement(By.tagName("h1")).isDisplayed();
+//		if(avail) {
+//			System.out.println("Testcase pass");
+//		}
+//		else {
+//			System.out.println("Testcase fail");
+//		}
+//		
+//		driver.close();
+//		
+
+		// getTagName()
+		// Test case 2
+
+//		String tagName = driver.findElement(By.cssSelector("[name='message']")).getTagName();
+//		if (tagName.equals("textarea")) {
+//			System.out.println("Testcase pass");
+//		} else {
+//			System.out.println("Test case fail");
+//		}
+//		
+//		driver.close();
+//	
+		// getCssValue()
+//		String getFontWeight = driver.findElement(By.cssSelector("[name='message']")).getCssValue("font-weight");
+//		System.out.println(getFontWeight);
+//		if(Integer.parseInt(getFontWeight) == 500) {
+//			System.out.println("Test case pass");
+//		}
+//		else {
+//			System.out.println("Test case fail");
+//		}
+//		driver.close();
 		
-		// Weblements 
+	
+		// getLocation()
 		
-		WebElement firstName = driver.findElement(By.cssSelector("input[name ='first_name']"));
-		WebElement lastName = driver.findElement(By.cssSelector("input[name ='last_name']"));
-		WebElement email = driver.findElement(By.cssSelector("input[name ='email']"));
-		WebElement message = driver.findElement(By.cssSelector("textarea[name ='message']"));
-		WebElement formButton = driver.findElement(By.cssSelector("input[type=\"submit\"]"));			 
-		WebElement form = driver.findElement(By.id("contact_form"));
+//		Point p  = driver.findElement(By.id("contact_form")).getLocation();
+//		System.out.println(p);
+//		driver.close();
 		
-		// sendKeys() , click() ,submit() , isDisplayed()
+		// getSize()
 		
-		firstName.sendKeys("chinmay");
-		lastName.sendKeys("deshpande");
-		email.sendKeys("chinmaydeshpande010@gmail.com");
-		message.sendKeys("I am learning js");
-		//formButton.click();
-		form.submit();
-		
-		
-		WebElement msg = driver.findElement(By.tagName("h1"));
-		boolean avail = msg.isDisplayed();
-		if(avail) {
-			System.out.println("testcase pass");
+		List<WebElement> classNameContact_Button = driver.findElements(By.className("contact_button"));
+		int count = classNameContact_Button.size();
+		if(count == 2) {
+			System.out.println("Testcase pass");
 		}
 		else {
-			System.out.println("testcase fail");
+			System.out.println("Test case fail");
 		}
 		
-		driver.close();
-		
-		
-		
-		
-		
+
+		// Next class
+		// findElement
+
+		// findElements
+
+		// getRect()
+
 	}
 
+	
 }
