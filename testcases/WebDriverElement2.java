9f7d8730778e8cb799b19e28f6ecf8919acaf1d9
minskole7 <minskole7@gmail.com>
2023-12-12 20:31:59 +0530

    added

---
+++ testcases/WebDriverElement2.java
@@ -0,0 +1,92 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverElement2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// program 1
+		// Test case 1
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Downloads\\chromedriver\\chromedriver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+//		driver.findElement(By.cssSelector("#content > div > ul > li:nth-child(1) > button")).click();
+//		
+//		String alertText = driver.switchTo().alert().getText();
+//		driver.switchTo().alert().accept();
+//		
+//		
+//		WebElement msgE = driver.findElement(By.id("result"));
+//		String msg = msgE.getText();
+//		
+//		if(msg.equals("You successfully clicked an alert") && alertText.equals("I am a JS Alert")){
+//			System.out.println("Test case pass");
+//		}
+//		else {
+//			System.out.println("Testcase fail");
+//		}
+		
+		// program 2
+		
+		
+		// Test case 2
+		
+//		driver.findElement(By.cssSelector("#content > div > ul > li:nth-child(3) > button")).click();
+//		driver.switchTo().alert().accept();
+//		String alertText2 = driver.switchTo().alert().getText();		
+//		WebElement msgE = driver.findElement(By.id("result"));
+//		String msg2 = msgE.getText();	
+//		if(msg2.equals("You clicked: Ok") && alertText2.equals("I am a JS Confirm")) {
+//				System.out.println("Test case pass");
+//		}
+//		else {
+//			System.out.println("Testcase fail");
+//		}
+		
+		
+		// Test case 3
+//		
+//		driver.findElement(By.cssSelector("#content > div > ul > li:nth-child(3) > button")).click();
+//		driver.switchTo().alert().dismiss();
+//		String alertText22 = driver.switchTo().alert().getText();		
+//		WebElement msgEe = driver.findElement(By.id("result"));
+//		String msg22 = msgEe.getText();	
+//		if(msg22.equals("You clicked: Cancel") && alertText22.equals("I am a JS Confirm")) {
+//				System.out.println("Test case pass");
+//		}
+//		else {
+//			System.out.println("Testcase fail");
+//		}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
