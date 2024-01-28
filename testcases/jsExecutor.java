3d7b914ccfecb8c97e3f69850c13711ec0684a14
minskole7 <minskole7@gmail.com>
2024-01-28 12:33:35 +0530

    added

---
+++ testcases/jsExecutor.java
@@ -0,0 +1,77 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class jsExecutor {
+
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+//		ChromeDriver driver;
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Downloads\\chromedriver21\\chromedriver\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.get("https://webdriveruniversity.com/");
+//		WebElement ele = driver.findElement(By.id("contact-us"));
+//		//ele.click();
+//		JavascriptExecutor js = (JavascriptExecutor)driver;
+//		js.executeScript("arguments[0].click()",ele);
+//		driver.quit();
+		
+		
+		
+		// Testcase 2
+//		ChromeDriver driver;
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Downloads\\chromedriver21\\chromedriver\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.get("https://webdriveruniversity.com/");
+//		WebElement ele = driver.findElement(By.id("popup-alerts"));
+//		JavascriptExecutor js = (JavascriptExecutor)driver;
+//		js.executeScript("arguments[0].scrollIntoView(true)",ele);
+//		//driver.quit();
+//		
+		
+		
+		// Testcase3
+//		ChromeDriver driver;
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Downloads\\chromedriver21\\chromedriver\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.get("https://webdriveruniversity.com/");
+//		JavascriptExecutor js = (JavascriptExecutor)driver;
+//		String title = (String)js.executeScript("return document.title");
+//		if(title.equals("WebDriverUniversity.com")) {
+//			System.out.println("Testcase pass");
+//		}
+//		else {
+//			System.out.println("Testcase fail");
+//		}
+//		driver.quit();
+		
+		
+		
+		// Test case 4
+//		ChromeDriver driver;
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Downloads\\chromedriver21\\chromedriver\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.get("https://webdriveruniversity.com/");
+//		JavascriptExecutor js = (JavascriptExecutor)driver;
+//		String titlee = (String)js.executeScript("return window.location.href");
+//		if(titlee.equals("https://webdriveruniversity.com/")) {
+//			System.out.println("Testcase pass");
+//		}
+//		else {
+//			System.out.println("Testcase fail");
+//		}
+//		driver.quit();
+//		
+		
+		
+		
+		
+
+	}
+
+}
